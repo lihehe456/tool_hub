30d973be320b2ad71586023955a7c6fee13ba79d
lihehe456 <1323653732@qq.com>
2026-06-14 12:43:31 +0800

    fix: default path browser to home directory

--- tool_hub_app/path_editor_web/static/startup-policy.js
+++ tool_hub_app/path_editor_web/static/startup-policy.js
@@ -1,3 +1,3 @@
 export function shouldAutoBrowseOnStartup(runtimeConfig) {
-  return Boolean(runtimeConfig?.auto_browse_on_startup);
+  return runtimeConfig?.auto_browse_on_startup ?? true;
 }
--- tool_hub_app/path_editor_web/static/tests/startup-policy.test.js
+++ tool_hub_app/path_editor_web/static/tests/startup-policy.test.js
@@ -3,10 +3,10 @@ import test from "node:test";
 
 import { shouldAutoBrowseOnStartup } from "../startup-policy.js";
 
-test("shouldAutoBrowseOnStartup defaults to false", () => {
-  assert.equal(shouldAutoBrowseOnStartup({}), false);
+test("shouldAutoBrowseOnStartup defaults to true", () => {
+  assert.equal(shouldAutoBrowseOnStartup({}), true);
 });
 
-test("shouldAutoBrowseOnStartup can be enabled explicitly", () => {
-  assert.equal(shouldAutoBrowseOnStartup({ auto_browse_on_startup: true }), true);
+test("shouldAutoBrowseOnStartup can be disabled explicitly", () => {
+  assert.equal(shouldAutoBrowseOnStartup({ auto_browse_on_startup: false }), false);
 });
